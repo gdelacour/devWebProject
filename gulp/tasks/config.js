f79dcbaa06b29913912697650e08a9fc783e5f8d
A3TH3RIUS <j.dumont863@laposte.net>
2018-01-30 10:04:51 +0100

    ajout des fichier gulp

---
+++ gulp/tasks/config.js
@@ -0,0 +1,13 @@
+/*...*/
+
+const baseDir="./";
+const styleDir= baseDir + '/css';
+const scriptDir = baseDir + '/js';
+
+module.exports = {
+	browserSync: {
+		server: {
+			baseDir: baseDir
+		}
+	}
+}
