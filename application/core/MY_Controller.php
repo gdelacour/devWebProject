92a3be119409f72b234cab57e9a19f3552f05a9e
A3TH3RIUS <j.dumont863@laposte.net>
2018-02-13 10:47:11 +0100

    modif My-C...

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,21 +1,30 @@
-class MY_Controller
+<?php
+
+class MY_Controller extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
     }
-    
-    public function renderView($view, $data = array()
-        {
-            $_data = array{
-                'siteTitle' => $this->config->item('siteTitle');
-                'pageTitle' => $this->config->item('pageTitle');
-            };
 
-            $_data = array_merge($_data, $data);
 
-            $this->load->view('global/header', $_data);
-            $this->load->view($view, $_data);
-            $this->load->view('global/footer');
-        }
+    /**
+    * Helpers to render composite view
+    * @param $view
+    * @param array $data
+    */
+
+    public function renderView($view, $data = array()){
+        $_data = array(
+            'siteTitle' => $this->config->item('siteTitle'),
+            'pageTitle' => $this->config->item('pageTitle'),
+        );
+
+        $_data = array_merge($_data, $data);
+
+        $this->load->view('global/header', $_data);
+        $this->load->view($view, $_data);
+        $this->load->view('global/footer');
+    }
 }
+?>
